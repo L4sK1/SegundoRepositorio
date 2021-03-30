38f4169b5fd218c4906409bc11b9b5221e7344b7
Lucas Augusto <lucaslaav38@gmail.com>
2021-03-30 12:38:17 -0300

    Adc getnome, setnome

---
+++ pessoa.java
@@ -0,0 +1,19 @@
+class Pessoa{
+    private String nome;
+    private String matricula;
+    private String sexo;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public Void setNome(nome){
+        this.nome = nome;
+    } 
+    
+    public inc calcularMatricula(matricula) {   
+        return matricula *10
+
+}
+
+}
